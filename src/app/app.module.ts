c6444dfa103390d9ab9ba7dfd3166a233c6b7ef7
Tyler Dinh <linhduydinh@gmail.com>
2018-04-23 22:42:51 +0700

    add question component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { AddQuestionComponent } from './add-question/add-question.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: AddQuestionComponent },
+  { path: 'add-question', component: AddQuestionComponent}
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddQuestionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
